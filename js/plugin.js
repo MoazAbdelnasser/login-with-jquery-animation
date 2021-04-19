d2a7002e575dbfd799c54486f96cf7cf5efa2144
Moaz_Abdelnasser <MoazAbdelnasser@users.github.com>
2021-04-19 14:35:12 +0200

    first commit

---
+++ js/plugin.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    "use Strict";
+     
+    $("h1").animate({top:"10px"},1000);
+    
+    $("button.float-left").animate({left:'0px'},500);
+    $("button.float-right").animate({right:'0px'},500);
+    $(".form-check").animate({bottom:'0px'},500,function(){
+        $(this).css("display","inline-block");
+    });
+   //Animate Company Logo
+    $(".login img").animate({top:"-80px"},500);
+    //Animate .welcome-visitor Message
+    $(".welcome-visitor").animate({opacity:'1'},500);
+});
